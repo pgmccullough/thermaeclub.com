c09044891e31a26d160527e6d2ce37ba0394136f
Patrick McCullough <p@mccullo.ug>
2025-04-09 11:08:19 -0400

    feat(ws): inject WS_SERVER_URL into client via root loader and window.ENV

--- app/root.tsx
+++ app/root.tsx
@@ -4,6 +4,7 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from '@remix-run/react'
 import type { LinksFunction } from '@remix-run/node'
 import { Socket } from 'socket.io-client'
@@ -44,6 +45,7 @@ export const links: LinksFunction = () => [
 ]
 
 export function Layout({ children }: { children: React.ReactNode }) {
+  const data = useLoaderData<typeof loader>()
   return (
     <html lang="en">
       <head>
@@ -55,6 +57,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <body>
         <Fathom />
         {children}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.ENV = ${JSON.stringify(data.ENV)};`,
+          }}
+        />
         <ScrollRestoration />
         <Scripts />
       </body>
@@ -78,4 +85,12 @@ export default function App() {
       <Outlet />
     </wsContext.Provider>
   )
+}
+
+export async function loader() {
+  return Response.json({
+    ENV: {
+      WS_SERVER_URL: process.env.WS_SERVER_URL,
+    },
+  })
 }
